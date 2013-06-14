563fa9fedbaab7f7e87e2703307090dde401c7c7
easyfly <yifei@zeroproc.com>
2013-06-14 14:19:13 +0800

    add1
    add2

--- 新建文件夹/stdafx.h
+++ 新建文件夹/stdafx.h
@@ -1,6 +1,6 @@
 // stdafx.h : 标准系统包含文件的包含文件，
 // 或是经常使用但不常更改的
-// 特定于项目的包含文件
+// 特定于项目的包含文件2
 //
 
 #pragma once
@@ -8,6 +8,7 @@
 #include "targetver.h"
 
 #include <stdio.h>
+
 #include <tchar.h>
 
 
